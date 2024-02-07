f5a0db12c442522d85e4642a796e967c17d6aaf9
Chirag-creator17 <chiraggoel172002@gmail.com>
2024-02-07 12:13:59 +0530

    added navbar and footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Navbar from "@/ui/navbar/navbar";
+import FooterComponent from "@/ui/footer/footer";
 import SearchLawyer from "@/ui/landing/searchLawyer";
 import FeaturesComponent from "@/ui/landing/featureComponent";
 import SearchLawyerComponent from "@/ui/landing/searchStateArea";
@@ -5,14 +7,18 @@ import DivAnimatedSteps from "@/ui/landing/steps";
 import Ecosystem from "@/ui/landing/ecosystem";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className="flex-auto mt-8">
-        <SearchLawyer />
-        <FeaturesComponent />
-        <DivAnimatedSteps />
-        <SearchLawyerComponent />
-        <Ecosystem />
-      </div>
-    </main>
+    <>
+      <Navbar />
+      <main className="flex min-h-screen flex-col items-center justify-between">
+        <div className="flex-auto mt-8">
+          <SearchLawyer />
+          <FeaturesComponent />
+          <DivAnimatedSteps />
+          <SearchLawyerComponent />
+          <Ecosystem />
+        </div>
+      </main>
+      <FooterComponent />
+    </>
   );
 }
