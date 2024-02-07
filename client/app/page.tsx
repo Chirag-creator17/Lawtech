6988d8e32806ffb9c9a14e389d7949660cf923f8
Chirag-creator17 <chiraggoel172002@gmail.com>
2024-02-07 12:38:46 +0530

    restructure

---
+++ client/app/page.tsx
@@ -0,0 +1,24 @@
+import Navbar from "@/ui/navbar/navbar";
+import FooterComponent from "@/ui/footer/footer";
+import SearchLawyer from "@/ui/landing/searchLawyer";
+import FeaturesComponent from "@/ui/landing/featureComponent";
+import SearchLawyerComponent from "@/ui/landing/searchStateArea";
+import DivAnimatedSteps from "@/ui/landing/steps";
+import Ecosystem from "@/ui/landing/ecosystem";
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <main className="flex min-h-screen flex-col items-center justify-between">
+        <div className="flex-auto mt-8">
+          <SearchLawyer />
+          <FeaturesComponent />
+          <DivAnimatedSteps />
+          <SearchLawyerComponent />
+          <Ecosystem />
+        </div>
+      </main>
+      <FooterComponent />
+    </>
+  );
+}
