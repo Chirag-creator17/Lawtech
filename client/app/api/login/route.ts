7d739348b51de1a6cef2e5a964125466eb1edaad
Chirag-creator17 <chiraggoel172002@gmail.com>
2024-02-20 10:51:16 +0530

    api trial

---
+++ client/app/api/login/route.ts
@@ -0,0 +1,7 @@
+import { NextRequest } from "next/server";
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+    console.log(body);
+    return new Response("success",{status:200});
+}
